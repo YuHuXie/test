71fd21f01b9d0a26f1f6f29400978f1639903e94
YuHUXie <yuhuxie2295@sina.com>
2018-07-27 17:36:01 +0800

    提交

---
+++ test4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test4
+//
+//  Created by wangqibin on 2018/7/27.
+//  Copyright © 2018年 wqb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test4
+//
+//  Created by wangqibin on 2018/7/27.
+//  Copyright © 2018年 wqb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
